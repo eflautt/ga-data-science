5105e796ae2552f81c5f28f465d7d082ef0d729b
Brandon M. Burroughs <brandonmburroughs@gmail.com>
2016-01-31 12:41:21 -0500

    Changing file path to data and updating Word2Vec import

--- lessons/lesson-14/code/starter-code/starter-code-14.py
+++ lessons/lesson-14/code/starter-code/starter-code-14.py
@@ -3,7 +3,7 @@ import spacy
 from spacy.en import English
 
 # Gensim is used for LDA and word2vec
-from gensim import Word2Vec
+from gensim.models.word2vec import Word2Vec
 
 # Write a function that can take a take a sentence parsed by `spacy` and 
 # identify if it mentions a company named 'Google'. 
@@ -19,7 +19,7 @@ from gensim import Word2Vec
 
 if __name__ == '__main__':
     # Loading the tweet data
-    tweets = [tweet for tweet in open('../../assets/data/captured-tweets.txt', 'r')]
+    tweets = [tweet for tweet in open('../../assets/dataset/captured-tweets.txt', 'r')]
 
     # Setting up spacy
     nlp_toolkit = English()
